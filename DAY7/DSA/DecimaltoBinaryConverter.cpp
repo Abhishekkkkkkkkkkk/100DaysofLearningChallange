d80ce373d248251a1a342917c434a66011601fcf
Abhishek kumar <109682053+Abhishekkkkkkkkkkk@users.noreply.github.com>
2024-05-12 16:46:58 +0530

    Day-7 Updated

--- DAY7/DSA/DecimaltoBinaryConverter.cpp
+++ DAY7/DSA/DecimaltoBinaryConverter.cpp
@@ -11,13 +11,9 @@ int main(){
     int i = 0;
 
     while (n != 0){
-      int digit = n % 10;
-      if(digit == 1){
-          ans = (ans + pow(2 , i));
-          
-      }
-        
-        n = n / 10;
+      int bit = n & 1;
+        ans = (bit * pow(10, i)) + ans;
+        n = n >> 1;
         i++;
 
     }
