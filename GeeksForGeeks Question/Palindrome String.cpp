c6fbabcf2f43a6872deb6caa98328cb04f153c70
Abhishek kumar <109682053+Abhishekkkkkkkkkkk@users.noreply.github.com>
2024-07-16 12:52:24 +0530

    Day 55 Core Subject Done

---
+++ GeeksForGeeks Question/Palindrome String.cpp
@@ -0,0 +1,19 @@
+// Given a string S, check if it is palindrome or not.
+
+// class Solution{
+// public:	
+	
+// 	int isPalindrome(string S)
+// 	{
+// 	    // Your code goes here
+// 	    int i = 0;
+//         int j = S.size() - 1;
+    
+//         while (i < j){
+            
+//             if (S[i++] != S[j--])
+//                 return false;
+//         }
+//         return true;
+//     }
+// };
