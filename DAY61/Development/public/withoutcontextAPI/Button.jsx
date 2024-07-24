8b470274207657c40b7c6f59b556497100f97d11
Abhishek kumar <109682053+Abhishekkkkkkkkkkk@users.noreply.github.com>
2024-07-24 15:26:05 +0530

    Web Dev Day 61 Done

---
+++ DAY61/Development/public/withoutcontextAPI/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Component1 from './Component1'
+
+const Button = ({count}) => {
+  return (
+    <div>
+      <button><span> <Component1 count={count}/> </span>I am a Button</button>
+    </div>
+  )
+}
+
+export default Button
---
+++ DAY61/Development/public/withoutcontextAPI/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "./Button";
+
+const Navbar = ({count}) => {
+  return (
+    <>
+      <div>Navbar</div>
+      <Button count={count}/>
+    </>
+  );
+};
+
+export default Navbar;
