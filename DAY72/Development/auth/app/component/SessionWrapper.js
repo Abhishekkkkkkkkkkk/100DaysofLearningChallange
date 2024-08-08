aa736b5c18a85bc0b8bcc9ab61398a8103bf0982
Abhishek kumar <109682053+Abhishekkkkkkkkkkk@users.noreply.github.com>
2024-08-08 12:46:21 +0530

    Web Dev Done

---
+++ DAY72/Development/auth/app/component/SessionWrapper.js
@@ -0,0 +1,14 @@
+"use client"
+import { SessionProvider } from "next-auth/react"
+
+
+
+const SessionWrapper = ({children}) => {
+  return (
+    <SessionProvider>
+        {children}
+    </SessionProvider>
+  )
+}
+
+export default SessionWrapper
